977cdbf150758d6690159827ef1cc85e078604e5
Verbovyi Dmytro <72096281+empty56@users.noreply.github.com>
2024-04-26 13:34:38 +0300

    Fixes MITZ_lab1.py

--- MITZ_lab1.py
+++ MITZ_lab1.py
@@ -1,8 +1,13 @@
+import sys
+
+lines = 19
+file_name = "input1.txt"
+
+
 def check_winner(board):
     directions = [(0, 1), (1, 0), (1, 1), (1, -1)]  # Right, Down, Diagonal, Anti-Diagonal
-
-    for i in range(19):
-        for j in range(19):
+    for i in range(lines):
+        for j in range(lines):
             if board[i][j] == 0:
                 continue
 
@@ -10,11 +15,10 @@ def check_winner(board):
                 consecutive_forward = count_consecutive(board, i, j, dx, dy)
                 consecutive_backward = count_consecutive(board, i, j, -dx, -dy)
                 total_consecutive = consecutive_forward + consecutive_backward - 1
-
                 if total_consecutive == 5 and (dx != 1 or dy != -1):
                     return board[i][j], i + 1, j + 1
                 if total_consecutive == 5 and dx == 1 and dy == -1:
-                    return board[i][j], i + 5, j -3
+                    return board[i][j], i + 5, j - 3
     return 0, 0, 0
 
 
@@ -22,7 +26,7 @@ def count_consecutive(board, row, col, dx, dy):
     consecutive = 0
     x, y = row, col
 
-    while 0 <= x < 19 and 0 <= y < 19 and board[x][y] == board[row][col]:
+    while 0 <= x < lines and 0 <= y < lines and board[x][y] == board[row][col]:
         consecutive += 1
         x += dx
         y += dy
@@ -30,12 +34,21 @@ def count_consecutive(board, row, col, dx, dy):
     return consecutive
 
 
-with open("input3.txt", "r") as file:
-    test_cases = int(file.readline().strip())
+def count_lines(file_path):
+    with open(file_path, 'r') as file:
+        line_count = sum(1 for line in file)
+    return line_count
 
+
+with open(file_name, "r") as file:
+    test_cases = int(file.readline().strip())
+    line_count = count_lines(file_name)
+    if (line_count - 1) % lines != 0:
+        print("Wrong number of lines")
+        sys.exit()
     for _ in range(test_cases):
         board = []
-        for _ in range(19):
+        for _ in range(lines):
             row = list(map(int, file.readline().split()))
             board.append(row)
 
